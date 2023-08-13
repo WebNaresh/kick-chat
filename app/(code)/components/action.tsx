83b1032f54ac0a7350bb9429a9331a403f5c133f
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-08-13 21:59:23 +0530

    bare-minimum

--- app/(code)/components/action.tsx
+++
@@ -1,7 +0,0 @@
-type Props = {};
-
-const Action = (props: Props) => {
-  return <div>Action</div>;
-};
-
-export default Action;
--- app/(code)/page.tsx
+++ app/(code)/page.tsx
@@ -1,17 +1,7 @@
 import type { NextPage } from "next";
-import Image from "next/image";
 
 const HomeComponent: NextPage = () => {
-  return (
-    <>
-      <Image
-        alt="Hero Image"
-        src={"/bg-12.jpg"}
-        width={1200}
-        height={500}
-      ></Image>
-    </>
-  );
+  return <></>;
 };
 
 export default HomeComponent;
