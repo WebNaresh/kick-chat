b7240f2e678fdc4d3a4d75b0465f1d877ceb5b0d
omkarkokil <nsjustice21@gmail.com>
2023-08-14 12:17:42 +0530

    adding Log in and sign up

---
+++ app/(code)/login/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import AuthForm from "./components/AuthForm";
+
+const page = () => {
+  return (
+    <>
+      <section className="flex h-[80vh]  flex-col items-center justify-center">
+        <div
+          id="container"
+          className="py-4 px-10 sm:mx-auto sm:w-full sm:max-w-md  bg-[#FBFBFB] rounded-lg  shadow-md shadow-black/5 dark:bg-slate-900 dark:shadow-black/10 "
+        >
+          <AuthForm />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default page;
